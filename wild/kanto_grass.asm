13daaf16dd94d8d77ede01922249e13bb1ac514b
ArthropodAssault <142094483+ArthropodAssault@users.noreply.github.com>
2023-08-19 16:15:49 -0500

    Add files via upload

---
+++ wild/kanto_grass.asm
@@ -0,0 +1,845 @@
+; Kanto Pokémon in grass
+
+KantoGrassWildMons:
+
+	def_grass_wildmons DIGLETTS_CAVE
+	db 4 percent, 2 percent, 8 percent ; encounter rates: morn/day/nite
+	; morn
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	; day
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	; nite
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	db 48, DUGTRIO
+	end_grass_wildmons
+
+	def_grass_wildmons MOUNT_MOON
+	db 6 percent, 6 percent, 6 percent ; encounter rates: morn/day/nite
+	; morn
+	db 61, CROBAT
+	db 60, RHYHORN
+	db 59, SANDSLASH
+	db 61, PARASECT
+	db 63, GOLEM
+	db 66, BLISSEY
+	db 62, CLEFABLE
+	; day
+	db 61, CROBAT
+	db 60, RHYHORN
+	db 59, SANDSLASH
+	db 61, PARASECT
+	db 63, GOLEM
+	db 66, BLISSEY
+	db 62, CLEFABLE
+	; nite
+	db 61, CROBAT
+	db 60, RHYHORN
+	db 59, SANDSLASH
+	db 61, PARASECT
+	db 63, GOLEM
+	db 66, BLISSEY
+	db 62, CLEFABLE
+	end_grass_wildmons
+
+	def_grass_wildmons ROCK_TUNNEL_1F
+	db 6 percent, 6 percent, 6 percent ; encounter rates: morn/day/nite
+	; morn
+	db 50, MAROWAK
+	db 51, GOLEM
+	db 52, MACHAMP
+	db 52, CROBAT
+	db 55, MACHAMP
+	db 52, MAROWAK
+	db 52, MAROWAK
+	; day
+	db 50, MAROWAK
+	db 51, GOLEM
+	db 52, MACHAMP
+	db 52, CROBAT
+	db 55, MACHAMP
+	db 52, MAROWAK
+	db 52, MAROWAK
+	; nite
+	db 50, MAROWAK
+	db 51, GOLEM
+	db 52, MACHAMP
+	db 52, CROBAT
+	db 55, MACHAMP
+	db 52, MAROWAK
+	db 52, MAROWAK
+	end_grass_wildmons
+
+	def_grass_wildmons ROCK_TUNNEL_B1F
+	db 6 percent, 6 percent, 6 percent ; encounter rates: morn/day/nite
+	; morn
+	db 50, MAROWAK
+	db 51, GOLEM
+	db 52, MACHAMP
+	db 52, CROBAT
+	db 55, MACHAMP
+	db 52, MAROWAK
+	db 52, MAROWAK
+	; day
+	db 50, MAROWAK
+	db 51, GOLEM
+	db 52, MACHAMP
+	db 52, CROBAT
+	db 55, MACHAMP
+	db 52, MAROWAK
+	db 52, MAROWAK
+	; nite
+	db 50, MAROWAK
+	db 51, GOLEM
+	db 52, MACHAMP
+	db 52, CROBAT
+	db 55, MACHAMP
+	db 52, MAROWAK
+	db 52, MAROWAK
+	end_grass_wildmons
+
+	def_grass_wildmons VICTORY_ROAD
+	db 6 percent, 6 percent, 6 percent ; encounter rates: morn/day/nite
+	; morn
+	db 40, STEELIX
+	db 41, TYRANITAR
+	db 42, RHYDON
+	db 43, URSARING
+	db 44, VAPOREON
+	db 42, ELECTABUZZ
+	db 41, CROBAT
+	; day
+	db 40, STEELIX
+	db 41, TYRANITAR
+	db 42, RHYDON
+	db 43, URSARING
+	db 44, VAPOREON
+	db 42, ELECTABUZZ
+	db 41, CROBAT
+	; nite
+	db 40, STEELIX
+	db 41, TYRANITAR
+	db 42, RHYDON
+	db 43, URSARING
+	db 44, VAPOREON
+	db 42, ELECTABUZZ
+	db 41, CROBAT
+	end_grass_wildmons
+
+	def_grass_wildmons TOHJO_FALLS
+	db 6 percent, 6 percent, 6 percent ; encounter rates: morn/day/nite
+	; morn
+	db 34, STEELIX
+	db 35, TYRANITAR
+	db 36, RHYDON
+	db 33, ARCANINE
+	db 32, VAPOREON
+	db 36, AMPHAROS
+	db 35, DRAGONITE
+	; day
+	db 34, STEELIX
+	db 35, TYRANITAR
+	db 36, RHYDON
+	db 33, ARCANINE
+	db 32, VAPOREON
+	db 36, AMPHAROS
+	db 35, DRAGONITE
+	; nite
+	db 34, STEELIX
+	db 35, TYRANITAR
+	db 36, RHYDON
+	db 33, ARCANINE
+	db 32, VAPOREON
+	db 36, AMPHAROS
+	db 35, DRAGONITE
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_1
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, PIDGEOT
+	db 66, RATICATE
+	db 63, FURRET
+	db 54, PIDGEOT
+	db 61, FURRET
+	db 62, PIDGEOT
+	db 65, FEAROW
+	; day
+	db 65, PIDGEOT
+	db 66, RATICATE
+	db 63, FURRET
+	db 54, PIDGEOT
+	db 61, FURRET
+	db 62, PIDGEOT
+	db 65, FEAROW
+	; nite
+	db 65, PIDGEOT
+	db 66, RATICATE
+	db 63, FURRET
+	db 54, PIDGEOT
+	db 61, FURRET
+	db 62, PIDGEOT
+	db 65, FEAROW
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_2
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, PIDGEOT
+	db 66, RATICATE
+	db 63, FURRET
+	db 54, PIDGEOT
+	db 61, FURRET
+	db 62, PIDGEOT
+	db 65, FEAROW
+	; day
+	db 65, PIDGEOT
+	db 66, RATICATE
+	db 63, FURRET
+	db 54, PIDGEOT
+	db 61, FURRET
+	db 62, PIDGEOT
+	db 65, FEAROW
+	; nite
+	db 65, PIDGEOT
+	db 66, RATICATE
+	db 63, FURRET
+	db 54, PIDGEOT
+	db 61, FURRET
+	db 62, PIDGEOT
+	db 65, FEAROW
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_3
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 61, CROBAT
+	db 60, RHYHORN
+	db 59, SANDSLASH
+	db 61, PARASECT
+	db 63, GOLEM
+	db 66, BLISSEY
+	db 62, CLEFABLE
+	; day
+	db 61, CROBAT
+	db 60, RHYHORN
+	db 59, SANDSLASH
+	db 61, PARASECT
+	db 63, GOLEM
+	db 66, BLISSEY
+	db 62, CLEFABLE
+	; nite
+	db 61, CROBAT
+	db 60, RHYHORN
+	db 59, SANDSLASH
+	db 61, PARASECT
+	db 63, GOLEM
+	db 66, BLISSEY
+	db 62, CLEFABLE
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_4
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, GIRAFARIG
+	db 60, STANTLER
+	db 60, ARBOK
+	db 64, RATICATE
+	db 65, ARBOK
+	db 63, SANDSLASH
+	db 64, SANDSLASH
+	; day
+	db 65, GIRAFARIG
+	db 60, STANTLER
+	db 60, ARBOK
+	db 64, RATICATE
+	db 65, ARBOK
+	db 63, SANDSLASH
+	db 64, SANDSLASH
+	; nite
+	db 65, GIRAFARIG
+	db 60, STANTLER
+	db 60, ARBOK
+	db 64, RATICATE
+	db 65, ARBOK
+	db 63, SANDSLASH
+	db 64, SANDSLASH
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_5
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, GIRAFARIG
+	db 60, STANTLER
+	db 60, ARBOK
+	db 64, RATICATE
+	db 65, ARBOK
+	db 63, SANDSLASH
+	db 64, SANDSLASH
+	; day
+	db 65, GIRAFARIG
+	db 60, STANTLER
+	db 60, ARBOK
+	db 64, RATICATE
+	db 65, ARBOK
+	db 63, SANDSLASH
+	db 64, SANDSLASH
+	; nite
+	db 65, GIRAFARIG
+	db 60, STANTLER
+	db 60, ARBOK
+	db 64, RATICATE
+	db 65, ARBOK
+	db 63, SANDSLASH
+	db 64, SANDSLASH
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_6
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, GIRAFARIG
+	db 60, STANTLER
+	db 60, ARBOK
+	db 64, RATICATE
+	db 65, ARBOK
+	db 63, SANDSLASH
+	db 64, SANDSLASH
+	; day
+	db 65, GIRAFARIG
+	db 60, STANTLER
+	db 60, ARBOK
+	db 64, RATICATE
+	db 65, ARBOK
+	db 63, SANDSLASH
+	db 64, SANDSLASH
+	; nite
+	db 65, GIRAFARIG
+	db 60, STANTLER
+	db 60, ARBOK
+	db 64, RATICATE
+	db 65, ARBOK
+	db 63, SANDSLASH
+	db 64, SANDSLASH
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_7
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, POLITOED
+	db 63, BELLOSSOM
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; day
+	db 65, BELLOSSOM
+	db 63, ARCANINE
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; nite
+	db 65, ARCANINE
+	db 63, AMPHAROS
+	db 64, NIDOKING
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_8
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, POLITOED
+	db 63, BELLOSSOM
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; day
+	db 65, BELLOSSOM
+	db 63, ARCANINE
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; nite
+	db 65, ARCANINE
+	db 63, AMPHAROS
+	db 64, NIDOKING
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_9
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, POLITOED
+	db 63, BELLOSSOM
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; day
+	db 65, BELLOSSOM
+	db 63, ARCANINE
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; nite
+	db 65, ARCANINE
+	db 63, AMPHAROS
+	db 64, NIDOKING
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_10_NORTH
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, POLITOED
+	db 63, BELLOSSOM
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; day
+	db 65, BELLOSSOM
+	db 63, ARCANINE
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; nite
+	db 65, ARCANINE
+	db 63, AMPHAROS
+	db 64, NIDOKING
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_11
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, POLITOED
+	db 63, BELLOSSOM
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; day
+	db 65, BELLOSSOM
+	db 63, ARCANINE
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; nite
+	db 65, ARCANINE
+	db 63, AMPHAROS
+	db 64, NIDOKING
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_13
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, POLITOED
+	db 63, BELLOSSOM
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; day
+	db 65, BELLOSSOM
+	db 63, ARCANINE
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; nite
+	db 65, ARCANINE
+	db 63, AMPHAROS
+	db 64, NIDOKING
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_14
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, POLITOED
+	db 63, BELLOSSOM
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; day
+	db 65, BELLOSSOM
+	db 63, ARCANINE
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; nite
+	db 65, ARCANINE
+	db 63, AMPHAROS
+	db 64, NIDOKING
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_15
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, POLITOED
+	db 63, BELLOSSOM
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; day
+	db 65, BELLOSSOM
+	db 63, ARCANINE
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; nite
+	db 65, ARCANINE
+	db 63, AMPHAROS
+	db 64, NIDOKING
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_16
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, POLITOED
+	db 63, BELLOSSOM
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; day
+	db 65, BELLOSSOM
+	db 63, ARCANINE
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; nite
+	db 65, ARCANINE
+	db 63, AMPHAROS
+	db 64, NIDOKING
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_17
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, POLITOED
+	db 63, BELLOSSOM
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; day
+	db 65, BELLOSSOM
+	db 63, ARCANINE
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; nite
+	db 65, ARCANINE
+	db 63, AMPHAROS
+	db 64, NIDOKING
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_18
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, POLITOED
+	db 63, BELLOSSOM
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; day
+	db 65, BELLOSSOM
+	db 63, ARCANINE
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; nite
+	db 65, ARCANINE
+	db 63, AMPHAROS
+	db 64, NIDOKING
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_21
+	db 6 percent, 6 percent, 6 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, POLITOED
+	db 63, BELLOSSOM
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; day
+	db 65, BELLOSSOM
+	db 63, ARCANINE
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; nite
+	db 65, ARCANINE
+	db 63, AMPHAROS
+	db 64, NIDOKING
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_22
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, POLITOED
+	db 63, BELLOSSOM
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; day
+	db 65, BELLOSSOM
+	db 63, ARCANINE
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; nite
+	db 65, ARCANINE
+	db 63, AMPHAROS
+	db 64, NIDOKING
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_24
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, POLITOED
+	db 63, BELLOSSOM
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; day
+	db 65, BELLOSSOM
+	db 63, ARCANINE
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; nite
+	db 65, ARCANINE
+	db 63, AMPHAROS
+	db 64, NIDOKING
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_25
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, POLITOED
+	db 63, BELLOSSOM
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; day
+	db 65, BELLOSSOM
+	db 63, ARCANINE
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; nite
+	db 65, ARCANINE
+	db 63, AMPHAROS
+	db 64, NIDOKING
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_26
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 34, POLITOED
+	db 35, BELLOSSOM
+	db 33, NIDOQUEEN
+	db 35, SNORLAX
+	db 33, STEELIX
+	db 32, RHYDON
+	db 32, JOLTEON
+	; day
+	db 34, BELLOSSOM
+	db 35, ARCANINE
+	db 33, NIDOQUEEN
+	db 35, SNORLAX
+	db 33, STEELIX
+	db 32, RHYDON
+	db 32, JOLTEON
+	; nite
+	db 34, ARCANINE
+	db 35, AMPHAROS
+	db 33, NIDOKING
+	db 35, SNORLAX
+	db 33, STEELIX
+	db 32, RHYDON
+	db 32, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_27
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 34, POLITOED
+	db 35, BELLOSSOM
+	db 33, NIDOQUEEN
+	db 35, SNORLAX
+	db 33, STEELIX
+	db 32, RHYDON
+	db 32, JOLTEON
+	; day
+	db 34, BELLOSSOM
+	db 35, ARCANINE
+	db 33, NIDOQUEEN
+	db 35, SNORLAX
+	db 33, STEELIX
+	db 32, RHYDON
+	db 32, JOLTEON
+	; nite
+	db 34, ARCANINE
+	db 35, AMPHAROS
+	db 33, NIDOKING
+	db 35, SNORLAX
+	db 33, STEELIX
+	db 32, RHYDON
+	db 32, JOLTEON
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_28
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 65, POLITOED
+	db 63, BELLOSSOM
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; day
+	db 65, BELLOSSOM
+	db 63, ARCANINE
+	db 64, NIDOQUEEN
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	; nite
+	db 65, ARCANINE
+	db 63, AMPHAROS
+	db 64, NIDOKING
+	db 64, SNORLAX
+	db 65, STEELIX
+	db 63, RHYDON
+	db 64, JOLTEON
+	end_grass_wildmons
+
+	db -1 ; end
---
+++ wild/treemons_asleep.asm
@@ -0,0 +1,31 @@
+; Used by CheckSleepingTreeMon
+
+AsleepTreeMonsNite:
+	db CATERPIE
+	db METAPOD
+	db BUTTERFREE
+	db WEEDLE
+	db KAKUNA
+	db BEEDRILL
+	db SPEAROW
+	db EKANS
+	db EXEGGCUTE
+	db LEDYBA
+	db AIPOM
+	db -1 ; end
+
+AsleepTreeMonsDay:
+	db BUTTERFREE
+	db DODUO
+	db NOCTOWL
+	db SPEAROW
+	db HERACROSS
+	db -1 ; end
+
+AsleepTreeMonsMorn:
+	db VENOMOTH
+	db PIDGEY
+	db NOCTOWL
+	db SPEAROW
+	db HERACROSS
+	db -1 ; end
